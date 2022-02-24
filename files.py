f288a83d9cccb19f7a9a92f5b33afb49779b35ae
Abhinav Sharma <abhinav291sharma@gmail.com>
2022-02-24 20:58:55 +0530

    3  NF

---
+++ files.py
@@ -0,0 +1,9 @@
+import pathlib
+
+print(pathlib.Path('soource/active/filenameabcd.txt').suffix)
+'''import os
+
+file_details = os.path.split('/bin/activate.ext')
+print(file_details)
+print(file_details[1])
+'''
---
+++ strings.py
@@ -0,0 +1,5 @@
+my_str = '''This is a multiline string.
+I am practising python now. It's good. But, I still prefer statically typed languages
+over dynamic languages'''
+
+print(my_str)
