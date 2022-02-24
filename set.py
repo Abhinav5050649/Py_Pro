06c8c5eeb57402659e7a22c62b5e69f3fae659b5
Abhinav Sharma <abhinav291sharma@gmail.com>
2022-02-24 23:51:26 +0530

    1 NF set.py

---
+++ set.py
@@ -0,0 +1,12 @@
+#Set operations
+s = {1, 2, 3}
+
+print(s) #Printing the entire set
+
+s.add(4) #Adding element into the set
+
+print(s) #Printing the modified set
+
+s.remove(4) #Removing element from set
+
+print(s) #Printing the set again
