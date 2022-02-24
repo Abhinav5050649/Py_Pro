14b1a72523968c8cccf99fa09b4f052f5fde144b
Abhinav Sharma <abhinav291sharma@gmail.com>
2022-02-24 16:33:56 +0530

    Added set_ops.py

---
+++ set_ops.py
@@ -0,0 +1,7 @@
+E = {2, 3, 1, 0, 4}
+U = {7, 8, 5, 6, 9}
+
+print("Union of E and U is: ", E | U)
+print("Intersection of E and U is: ", E & U)
+print("Set difference of E and U: ", E - U)
+print("Symmetric set difference of E and U: ", E ^ U)
